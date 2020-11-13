e259accb33b802f42dbd5d453e8b72e446086470
hc3g17 <hc3g17@soton.ac.uk>
2020-11-13 11:20:31 +0000

    added lib files/scripts back, apparently they vanished

---
+++ NN_algorithms/make_lib_riscv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export RISCV_LINK_LIB=${PWD}/lib
+
+mkdir $RISCV_LINK_LIB 
+
+cd $CV32SIM
+cd ../../
+cd bsp
+make
+cp libcv-verif.a $RISCV_LINK_LIB/libcv-verif.a
+cp link.ld $RISCV_LINK_LIB/link.ld
+
+cd $RISCV_LINK_LIB
+cp /opt/riscv/lib/gcc/riscv32-unknown-elf/9.2.0/crtbegin.o crtbegin.o
+
+ls
