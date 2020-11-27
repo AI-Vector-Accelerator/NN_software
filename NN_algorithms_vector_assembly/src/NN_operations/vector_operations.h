4d01934e73728837da9d895d139411b4a64453cd
hc3g17 <hc3g17@soton.ac.uk>
2020-11-27 18:24:22 +0000

    Fixed assembly for TFlite conv function

--- NN_algorithms_vector_assembly/src/NN_operations/vector_operations.h
+++ NN_algorithms_vector_assembly/src/NN_operations/vector_operations.h
@@ -1,6 +1,10 @@
 #ifndef VECTOR_OPERATIONS_H_
 #define VECTOR_OPERATIONS_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
 
 #define MAX_INT8_T 127
@@ -21,8 +25,8 @@ extern void vect_max_stride_vec2(unsigned int N, const int8_t *vec1,const int8_t
 extern void vect_dotProduct(unsigned int N, const int8_t *vec1, const int8_t *vec2, int32_t *scalarOut);
 extern void vect_dotProduct_offset(unsigned int N, const int8_t *vec1, const int8_t *vec2, int32_t *scalarOut, const int8_t vec1Offset, const int8_t vec2Offset);
 extern void vect_dotProduct_stride_vec2(unsigned int N, const int8_t *vec1, const int8_t *vec2, int32_t *scalarOut, uint32_t stride);
-extern void vect_dotProduct_offset_stride_vec2(unsigned int N, const int8_t *vec1, const int8_t *vec2, int32_t *scalarOut, const int8_t vec1Offset, const int8_t vec2Offset, uint32_t stride);
-extern void vectu_dotProduct_offset_stride_vec2(unsigned int N, const uint8_t *vec1, const uint8_t *vec2, int32_t *scalarOut, const int8_t vec1Offset, const int8_t vec2Offset, uint32_t stride);
+extern void vect_dotProduct_offset_stride(unsigned int N, const int8_t *vec1, const int8_t *vec2, int32_t *scalarOut, const int8_t vec1Offset, const int8_t vec2Offset, uint32_t vec1Stride, uint32_t vec2Stride);
+extern void vectu_dotProduct_offset_stride(unsigned int N, const uint8_t *vec1, const uint8_t *vec2, int32_t *scalarOut, const int8_t vec1Offset, const int8_t vec2Offset, uint32_t vec1Stride, uint32_t vec2Stride);
 
 
 extern void vect_ReLu(unsigned int N, const int8_t *vec1, int8_t *vecOut);
@@ -38,5 +42,9 @@ extern void vect_copy_reg(unsigned int N,const int8_t value, int8_t *vecOut);
 int8_t saturate_16bit_to_8bit(int16_t input);
 int8_t saturate_32bit_to_8bit(int32_t input);
 
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* VECTOR_OPERATIONS_H_ */
