067af84158f4e74977eec681c285fec9323009d9
hc3g17 <hc3g17@soton.ac.uk>
2020-12-03 19:00:17 +0000

    added vector assembly to support pooling and add functions in TF

--- NN_algorithms_vector_assembly/src/NN_operations/vector_operations.h
+++ NN_algorithms_vector_assembly/src/NN_operations/vector_operations.h
@@ -13,13 +13,19 @@ extern "C" {
 extern void vect_init(void);
 
 extern void vect_add(unsigned int N, const int8_t *vec1, const int8_t *vec2, int8_t *vecOut);
+extern void vect_add_32bits(unsigned int N,  int32_t *vec1,  int32_t *vec2, int32_t *vecOut);
+extern void vect_addElementWise(unsigned int N, const int8_t *vec1, int32_t *vecOut, const int32_t vec1Offset, const int32_t scale);
 extern void vect_mult(unsigned int N, const int8_t *vec1, const int8_t *vec2, int8_t *vecOut);
 
 extern void vect_addReduction(unsigned int N, const int8_t *vec1, int16_t *scalarOut);
+extern void vect_addReduction_stride(unsigned int N, const int8_t *vec1, int16_t *scalarOut, const int16_t scalarIn, uint32_t stride);
+extern void vectu_addReduction_stride(unsigned int N, const uint8_t *vec1, uint16_t *scalarOut, const uint16_t scalarIn, uint32_t stride);
 extern void vect_add_stride_vec2(unsigned int N, const int16_t *vec1,const int8_t *vec2, int16_t *vecOut, uint32_t stride);
 
 
 extern void vect_maxReduction(unsigned int N, const int8_t *vec1, int8_t *scalarOut);
+extern void vect_maxReduction_stride(unsigned int N, const int8_t *vec1, int8_t *scalarOut, const int8_t scalarIn, uint32_t stride);
+extern void vectu_maxReduction_stride(unsigned int N, const uint8_t *vec1, uint8_t *scalarOut, const uint8_t scalarIn, uint32_t stride);
 extern void vect_max_stride_vec2(unsigned int N, const int8_t *vec1,const int8_t *vec2, int8_t *vecOut, uint32_t stride);
 
 
@@ -35,6 +41,7 @@ extern void vect_ReLu(unsigned int N, const int8_t *vec1, int8_t *vecOut);
 extern void vect_ReLu_Bound(unsigned int N, const int8_t *vec1, int8_t *vecOut, int8_t lowerBound);
 extern void vect_ReLu6(unsigned int N, const int8_t *vec1, int8_t *vecOut);
 extern void vect_ReLu6_Bound(unsigned int N, const int8_t *vec1, int8_t *vecOut, int8_t lowerBound, int8_t upperBound);
+extern void vect_ReLu6_Bound_32bits(unsigned int N,  int32_t *vec1, int32_t *vecOut, int32_t lowerBound, int32_t upperBound);
 extern void vect_ReLu6_Bound_unsigned(unsigned int N, const uint8_t *vec1, uint8_t *vecOut, uint8_t lowerBound, uint8_t upperBound);
 
 extern void vect_copy(unsigned int N,const int8_t *vec1, int8_t *vecOut);
