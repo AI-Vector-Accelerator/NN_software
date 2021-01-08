9c4c438a5088e133f0e0949143f4cbd0c16d3a84
hc3g17 <hc3g17@soton.ac.uk>
2021-01-08 11:29:04 +0000

    changed riscv lin make script

--- NN_algorithms/make_lib_riscv.sh
+++ NN_algorithms/make_lib_riscv.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 export RISCV_LINK_LIB=${PWD}/lib
 
-mkdir $RISCV_LINK_LIB 
+mkdir -p $RISCV_LINK_LIB 
 
-cd /opt/core-v-verif/cv32/bsp
-make
-cp libcv-verif.a $RISCV_LINK_LIB/libcv-verif.a
-cp link.ld $RISCV_LINK_LIB/link.ld
+cp $CV32SIM/../../bsp/libcv-verif.a $RISCV_LINK_LIB/libcv-verif.a
+cp $CV32SIM/../../bsp/link.ld $RISCV_LINK_LIB/link.ld
 
-cd $RISCV_LINK_LIB
-cp /opt/riscv/lib/gcc/riscv32-unknown-elf/9.2.0/crtbegin.o crtbegin.o
+cp $RISCV/lib/gcc/riscv32-unknown-elf/9.2.0/crtbegin.o $RISCV_LINK_LIB/crtbegin.o
 
-ls
+ls $RISCV_LINK_LIB 
+ 
